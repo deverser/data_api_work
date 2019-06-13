1a76cee950e68a37ace92ee44f523b22a9940b35
Alexander <alexmal74@gmail.com>
2019-06-13 11:59:40 +0500

    Extended visualization of the most popular Python repos on GitHub using pygal library

--- python_repos.py
+++ python_repos.py
@@ -1,4 +1,6 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 
 # Создание вызова API и сохранение ответа
@@ -12,15 +14,40 @@ print("Total repositories: ", response_dict['total_count'])
 
 # Анализ информации о репозиториях
 repo_dicts = response_dict['items']
-print("Repositories returned: ", len(repo_dicts))
+print("Number of items: ", len(repo_dicts))
 
-print("\nSelected information about each repository: ")
+
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
-    print('\nName: ', repo_dict['name'])
-    print('Owner: ', repo_dict['owner']['login'])
-    print('Stars: ', repo_dict['stargazers_count'])
-    print('Repository: ', repo_dict['html_url'])
-    # print('Created: ', repo_dict['created_at'])
-    # print('Updated: ', repo_dict['updated_at'])
-    print('Description: ', repo_dict['description'])
+    names.append(repo_dict['name'])
+    # Извлечение описания проекта, если таковое имеется в полученных данных
+    description = repo_dict['description']
+    if not description:
+        description = 'No description provided'
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': description,
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
 
+
+# Построение визуализации
+my_style = LS('#333366', base_style=LCS)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
