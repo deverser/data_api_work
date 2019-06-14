4f4d91a2892f36a80a2ac80591e7c5a93e6c02e2
Alexander <alexmal74@gmail.com>
2019-06-14 15:09:56 +0500

    Fixed tests for python_repos.py

--- test_python_repos.py
+++ test_python_repos.py
@@ -1,24 +1,34 @@
 import unittest
-from python_repos import r, repo_dicts, response_dict
+import python_repos as pr
 
 class PythonReposTestCase(unittest.TestCase):
     """
     Тест для проверки статус кода и количества возвращаемых
     элементов при выполнении запроса API
     """
-    def test_status_code(self):
-        # Статус код
-        test_code = r.status_code
-        self.assertEqual(test_code, 200)
+    def setUp(self):
+        self.r = pr.get_response()
+        self.response_dict = self.r.json()
+        self.repo_dicts = pr.get_repo_dicts(self.r)
+        self.repo_dict = self.repo_dicts[0]
+        self.names, self.plot_dicts = pr.get_names_plot_dicts(self.repo_dicts)
 
-    def test_repodicts(self):
+
+    def test_get_response(self):
+        # Статус код
+        self.assertEqual(self.r.status_code, 200)
+
+    def test_repo_dicts(self):
         # Возвращаемые элементы
-        test_repos = len(repo_dicts)
-        self.assertEqual(test_repos, 30)
+        self.assertEqual(len(self.repo_dicts), 30)
+        # Словарь репозитория должен содержать следующие ключи
+        required_keys = ['name', 'owner', 'stargazers_count', 'html_url']
+        for key in required_keys:
+            self.assertTrue(key in self.repo_dict.keys())
 
     def test_total_count(self):
-        test_count = response_dict['total_count']
-        self.assertGreater(test_count, 3000000)
+        # Общее количество репозиториев должно быть больше определенного числа
+        self.assertGreater(self.response_dict['total_count'], 3000000)
 
 if __name__ == '__main__':
     unittest.main()
